6012c61671fee47ab5934875498dc88678b340b7
Said <zitouni.sd@gmail.com>
2021-06-30 16:27:38 +0100

    create Posts and Pages controller and setup first page and layouts folder(footer and app)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+<div>
+    <div>
+        <div></div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [PagesController::class,'index']);
 
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 
